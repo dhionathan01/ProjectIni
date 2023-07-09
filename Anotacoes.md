07ccf47d582959f9afac5908d900b1ec7018c79e
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2023-07-09 12:16:22 -0300

    docs: :memo: Anotação e teste e testando estrutura

--- Anotacoes.md
+++ Anotacoes.md
@@ -1,3 +1,4 @@
 # Anotações
 
 * Tudo que está dentro da pasta ```app/``` é uma rota.
+* o arquivo {layout.tsx} dentro de ```app/layout.tsx``` contém tudo que vai ficar em volta das páginas que vão se redenizadas
--- sistema_reserva_viagens/app/layout.tsx
+++ sistema_reserva_viagens/app/layout.tsx
@@ -16,7 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <h1>Sempre será exibido e ficara envolta da página</h1>
+        {children}
+      </body>
     </html>
   )
 }
