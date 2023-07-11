6094433470ba74390242eeeab5f92b086e26a4df
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2023-07-10 23:07:44 -0300

    build: :tada: Começando projeto

--- sistema_reserva_viagens/app/about/components/Viagens.tsx
+++ sistema_reserva_viagens/app/about/components/Viagens.tsx
@@ -3,17 +3,10 @@ import React, {useState, useEffect} from "react";
 
 
 const Viagens =  () => {
-    const [data, setData] = useState([]);
-    useEffect(() => {
-        fetch("http://jsonplaceholder.typicode.com/posts")
-            .then((res) => res.json())
-            .then((res) => setData(res));
-    }, []);
+  
     return (
         <div>
-            {data.map((i: any) => (
-                <p key={i.id}>{ i.title}</p>
-            ))}
+
         </div>
     );
 };
