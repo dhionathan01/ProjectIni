bd509522513c54c63488a79ac660054450a78d72
Dhionathan L. Jobim <dhionathan01@hotmail.com>
2023-07-19 00:32:15 -0300

    feat: :sparkles: Criando botão de logar e deslogar com conta do google

--- sistema_reserva_viagens/app/components/Header.tsx
+++ sistema_reserva_viagens/app/components/Header.tsx
@@ -1,11 +1,21 @@
+'use client';
 import React from "react";
 import Image from "next/image";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 const Header = () => {
+    const { status } = useSession();
+    const logarComClick = () => signIn();
+    const deslogarComClick = () => signOut();
     return (
         < div className="container mx-auto p-5 flex justify-between">
             <Image width={183} height={32} src="/logo.png" alt="Logo" />
-            <button className="text-primaria text-sm font-semibold"> Login </button>
+            {status == "unauthenticated" && (
+                <button className="text-primaria text-sm font-semibold" onClick={logarComClick}> Login </button>
+            )}
+            {status == "authenticated" && (
+                <button className="text-primaria text-sm font-semibold" onClick={deslogarComClick}> Logout </button>
+            )}
         </div>
     );
 };
